ae44309d6d517829d5abc80306b851838f485616
thohuynh1512 <thohuynh1512@gmail.com>
2019-05-24 20:23:10 +0700

    completed

---
+++ Operators_2.cpp
@@ -0,0 +1,16 @@
+﻿/*
+Viết chương trình C++ để nhắc người dùng nhập tên của họ, 
+sau đó in kết quả trên màn hình.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char ten[200];
+	cout << " Moi ban nhap ten: ";
+	cin >> ten;
+	cout << "Ten cua ban la: " << ten << endl;
+}
+
+
